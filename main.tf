0b374a6f3d9a95f889b273a31de906e274f5b453
Patrick <pcarey@hashicorp.com>
2021-10-29 15:25:08 +1100

    update pool config

--- main.tf
+++ main.tf
@@ -30,11 +30,19 @@ resource "bigip_ltm_monitor" "monitor" {
   parent = "/Common/http"
 }
 resource "bigip_ltm_pool" "pool" {
-  name                   = "/Common/Axiom_Environment_APP1_Pool"
+  name                   = "/Common/Region1_Pool"
   load_balancing_mode    = "round-robin"
   minimum_active_members = 1
   monitors               = [bigip_ltm_monitor.monitor.name]
 }
+
+resource "bigip_ltm_pool" "second_pool" {
+  name                   = "/Common/Region2_Pool"
+  load_balancing_mode    = "round-robin"
+  minimum_active_members = 1
+  monitors               = [bigip_ltm_monitor.monitor.name]
+}
+
 
 resource "bigip_as3" "as3-example1" {
   as3_json = file("example1.json")
